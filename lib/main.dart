bb539909c16fcb13d66a6ecdd21a31b368f5b604
hkhy2145 <132103457+hkhy2145@users.noreply.github.com>
2023-10-28 21:10:28 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -8,13 +10,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Hello World App', // Replace with your app name
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World'),
-        ),
-        body: Center(
-          child: Text('Hello, World!'),
+      home: HelloWorldApp(),
+    );
+  }
+}
+
+class HelloWorldApp extends StatefulWidget {
+  @override
+  _HelloWorldAppState createState() => _HelloWorldAppState();
+}
+
+class _HelloWorldAppState extends State<HelloWorldApp> {
+  final webhookUrl =
+      'YOUR_DISCORD_WEBHOOK_URL'; // Replace with your Discord webhook URL
+  String message = 'Hello, World!';
+  String displayMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    sendMessageToWebhook();
+  }
+
+  void sendMessageToWebhook() async {
+    await sendToDiscordWebhook(message);
+    await Future.delayed(Duration(seconds: 3));
+    setState(() {
+      displayMessage = 'Goodbye, World!';
+    });
+  }
+
+  Future<void> sendToDiscordWebhook(String message) async {
+    final response = await http.post(
+      Uri.parse(webhookUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'content': message}),
+    );
+
+    if (response.statusCode == 200) {
+      print('Message sent to Discord successfully');
+    } else {
+      print('Failed to send message to Discord: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              displayMessage,
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
         ),
       ),
     );
