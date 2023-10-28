4a668fc0547921643f798b83ef6fe4f7804ec67c
hkhy2145 <132103457+hkhy2145@users.noreply.github.com>
2023-10-28 02:45:34 +0100

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
-import 'package:app/function.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http; // Import the http package
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,11 +11,15 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   String url = '';
   var data;
-  String output = 'Initial_Output';
+  String output = 'Initial Output';
+
+  // Define your Discord webhook URL
+  final webhookUrl = 'https://discord.com/api/webhooks/1165290854416646225/NFI2Puw2SYeWNetzEm9sr_KtCSjEA-6CS54hTQZDCy7LD-EYLuv0rM2oioO7ObazFZvU';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Simple Flask App')),
+      appBar: AppBar(title: Text('Flutter Discord Webhook')),
       body: Center(
         child: Container(
           padding: EdgeInsets.all(20),
@@ -26,23 +30,45 @@ class _HomeState extends State<Home> {
               },
             ),
             TextButton(
-                onPressed: () async {
-                  data = await fetchdata('https://api64.ipify.org?format=json');
-                  setState(() {
-                    output = 'goooooood';
-                  });
-                },
-                child: Text(
-                  'Fetch ASCII Value',
-                  style: TextStyle(fontSize: 20),
-                )),
+              onPressed: () async {
+                data = await fetchdata('https://api64.ipify.org?format=json');
+                var decoded = jsonDecode(data);
+
+                // Send a message to the Discord webhook
+                await sendToDiscordWebhook('Message: ${decoded['output']}');
+
+                setState(() {
+                  output = 'Message sent to Discord!';
+                });
+              },
+              child: Text(
+                'Send Message to Discord',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
             Text(
               output,
-              style: TextStyle(fontSize: 40, color: Colors.green),
+              style: TextStyle(fontSize: 20, color: Colors.green),
             )
           ]),
         ),
       ),
     );
   }
+
+  Future<void> sendToDiscordWebhook(String message) async {
+    final response = await http.post(
+      Uri.parse(webhookUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'content': message}),
+    );
+
+    if (response.statusCode == 200) {
+      print('Message sent to Discord successfully');
+    } else {
+      print('Failed to send message to Discord: ${response.statusCode}');
+    }
+  }
+
+  // Your fetchdata and other methods go here
 }
