6d1b273480bd272e26bcb96edb641ee70fed3d1c
hkhy2145 <132103457+hkhy2145@users.noreply.github.com>
2023-10-30 12:30:11 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+Future<void> sendToDiscordWebhook() async {
+  String message = "jhgy";
+  String webhookUrl =
+      "https://discord.com/api/webhooks/1165290854416646225/NFI2Puw2SYeWNetzEm9sr_KtCSjEA-6CS54hTQZDCy7LD-EYLuv0rM2oioO7ObazFZvU";
+  final Map<String, String> headers = {'Content-Type': 'application/json'};
+  final Map<String, dynamic> data = {'content': message};
+  final String jsonData = json.encode(data);
+
+  final response = await http.post(
+    Uri.parse(webhookUrl),
+    headers: headers,
+    body: jsonData,
+  );
+
+  if (response.statusCode == 204) {
+    print('Message sent successfully to Discord webhook');
+  } else {
+    print(
+        'Failed to send message to Discord webhook. Status code: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
+}
+
 void main() {
   runApp(MyApp());
 }
@@ -24,29 +47,6 @@ class _DiscordWebhookAppState extends State<DiscordWebhookApp> {
   TextEditingController textEditingController = TextEditingController();
   String outputText = '';
 
-  void sendToDiscord() async {
-    String message = textEditingController.text;
-    String webhookUrl = "https://discord.com/api/webhooks/1165290854416646225/NFI2Puw2SYeWNetzEm9sr_KtCSjEA-6CS54hTQZDCy7LD-EYLuv0rM2oioO7ObazFZvU";
-
-    Map<String, String> data = {"content": message};
-
-    final response = await http.post(
-      Uri.parse(webhookUrl),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(data),
-    );
-
-    if (response.statusCode == 204) {
-      setState(() {
-        outputText = "Message sent to Discord successfully";
-      });
-    } else {
-      setState(() {
-        outputText = "Failed to send message to Discord: ${response.statusCode}";
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,7 +64,7 @@ class _DiscordWebhookAppState extends State<DiscordWebhookApp> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: sendToDiscord,
+              onPressed: sendToDiscordWebhook,
               child: Text('Send to Discord'),
             ),
             SizedBox(height: 20),
